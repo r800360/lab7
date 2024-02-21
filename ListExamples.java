34e77c02dc36370bebcd06116f5b985fd1950e9a
Rohan Sachdeva <rosachdeva@ieng6-201.ucsd.edu>
2024-02-21 11:15:29 -0800

    Changed new to new new

--- ListExamples.java
+++ ListExamples.java
@@ -5,7 +5,7 @@ interface StringChecker { boolean checkString(String s); }
 
 class ListExamples {
 
-  // Returns a new list that has all the elements of the input list for which
+  // Returns a new new list that has all the elements of the input list for which
   // the StringChecker returns true, and not the elements that return false, in
   // the same order they appeared in the input list;
   static List<String> filter(List<String> list, StringChecker sc) {
